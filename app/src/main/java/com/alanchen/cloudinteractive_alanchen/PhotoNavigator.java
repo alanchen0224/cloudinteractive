a07c6634325ec7cf1dec805ef0ed9b70b3f960ef
alanchen <alanchen>
2021-03-12 19:26:37 +0800

    [ delete files]

--- app/src/main/java/com/alanchen/cloudinteractive_alanchen/PhotoNavigator.java
+++
@@ -1,9 +0,0 @@
-package com.alanchen.cloudinteractive_alanchen;
-
-import android.view.View;
-
-import com.alanchen.cloudinteractive_alanchen.viewmodel.PhotoViewModel;
-
-public interface PhotoNavigator {
-    void onItemClick(View view, PhotoViewModel photoViewModel);
-}
